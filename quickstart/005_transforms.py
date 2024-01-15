088b5e20dd5a763aa1223a08bc67ebc56b80230d
c4fun <pcfeng502@gmail.com>
2024-01-15 12:33:39 +0800

    using ToTensor and Lambda to transform

---
+++ quickstart/005_transforms.py
@@ -0,0 +1,14 @@
+import torch
+from torchvision import datasets
+from torchvision.transforms import ToTensor, Lambda
+
+# transform: the features as normalized tensors, 
+# target_transform: and the labels as one-hot encoded tensors. 
+# Lambda transforms apply any user-defined lambda function. Here, we define a function to turn the integer into a one-hot encoded tensor. It first creates a zero tensor of size 10 (the number of labels in our dataset) and calls scatter_ which assigns a value=1 on the index as given by the label y
+ds = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+    target_transform=Lambda(lambda y: torch.zeros(10, dtype=torch.float).scatter_(0, torch.tensor(y), value=1))
+)
